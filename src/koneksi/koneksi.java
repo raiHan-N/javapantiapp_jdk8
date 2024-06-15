b2a487badac2746db668a91ea3a8b9fa1dc95cc5
raihan <ray102303@gmail.com>
2024-06-15 23:38:06 +0700

    first commit

---
+++ src/koneksi/koneksi.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package koneksi;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Raihan
+ */
+public class koneksi {
+     private Connection koneksi;
+    public Connection connect(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Berhasil Koneksi");
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Gagal Koneksi"+ ex);
+        }
+        String url = "jdbc:mysql://localhost/panti_app";
+        try {
+            koneksi = DriverManager.getConnection(url,"root","");
+            System.out.println("Berhasil Koneksi Database");
+        } catch (SQLException ex) {
+            System.out.println("Gagal Koneksi Database" + ex);
+        }
+        return koneksi;
+    }
+}
